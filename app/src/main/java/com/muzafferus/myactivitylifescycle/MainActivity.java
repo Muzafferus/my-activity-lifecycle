653e402fee2c809eb262dbf9d2d86b3a36cdcec0
muzafferus <muzefferpasazade@gmail.com>
2019-01-27 13:31:09 +0400

    add log

--- app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
+++ app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
@@ -3,8 +3,10 @@ package com.muzafferus.myactivitylifescycle;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.e("ActivityLifecycle", "onCreate");
         buttonDialog = findViewById(R.id.button_dialog);
         buttonOpenActivity = findViewById(R.id.button_one);
         buttonOpenForResult = findViewById(R.id.button_two);
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle(R.string.dialog_header);
                 builder.setMessage(R.string.dialog_body);
-                builder.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener(){
+                builder.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.dismiss();
                     }
@@ -58,5 +60,47 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 666);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("ActivityLifecycle", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("ActivityLifecycle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("ActivityLifecycle", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("ActivityLifecycle", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("ActivityLifecycle", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("ActivityLifecycle", "onRestart");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("ActivityLifecycle", "onActivityResult");
     }
 }
