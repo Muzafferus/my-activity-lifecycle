dbf7a399a450525f35a7903a84c8f82b1b047f5a
muzafferus <muzefferpasazade@gmail.com>
2019-01-27 13:06:44 +0400

    add activity change and dialog

--- app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
+++ app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
@@ -1,6 +1,9 @@
 package com.muzafferus.myactivitylifescycle;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -21,21 +24,38 @@ public class MainActivity extends AppCompatActivity {
         buttonDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.dialog_header);
+                builder.setMessage(R.string.dialog_body);
+                builder.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
 
+                builder.setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
             }
         });
 
         buttonOpenActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
 
         buttonOpenForResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 666);
             }
         });
     }
