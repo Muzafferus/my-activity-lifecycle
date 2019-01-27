e9d5510dfe75ee6d9a4aecc5f3c6f5d329d6e007
muzafferus <muzefferpasazade@gmail.com>
2019-01-27 12:37:23 +0400

    add 3 button and onClickListener

--- app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
+++ app/src/main/java/com/muzafferus/myactivitylifescycle/MainActivity.java
@@ -1,13 +1,42 @@
 package com.muzafferus.myactivitylifescycle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonDialog, buttonOpenActivity, buttonOpenForResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonDialog = findViewById(R.id.button_dialog);
+        buttonOpenActivity = findViewById(R.id.button_one);
+        buttonOpenForResult = findViewById(R.id.button_two);
+
+        buttonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonOpenActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonOpenForResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
